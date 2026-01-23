5baf65e8718318be26c2126c36194835d6e22d7b
RiccGHG <richard10hahn@gmail.com>
2026-01-23 19:53:21 +0000

    pls hud screen

---
+++ bp/scripts/hud/timer.js
@@ -0,0 +1,13 @@
+import { world, system } from "@minecraft/server";
+/**
+ * @typedef {Object} timer
+ * @property {number} h
+ * @property {number} min
+ * @property {number} sec
+ */
+/**@type {timer} */
+const timeObj = {
+    h: 0,
+    min: 0,
+    sec: 0
+}
--- bp/scripts/test.js
+++
@@ -1,5 +0,0 @@
-import { world } from "@minecraft/server";
-
-export function test(name) {
-world.sendMessage("Hello, " + name);
-}
