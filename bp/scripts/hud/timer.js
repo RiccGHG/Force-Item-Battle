37beaab5566d2abb499e97abc9a335ac4d956009
RiccGHG <richard10hahn@gmail.com>
2026-01-23 20:19:28 +0000

    .

--- bp/scripts/hud/timer.js
+++ bp/scripts/hud/timer.js
@@ -7,7 +7,23 @@ import { world, system } from "@minecraft/server";
  */
 /**@type {timer} */
 const timeObj = {
-    h: 0,
-    min: 0,
-    sec: 0
+  h: 0,
+  min: 0,
+  sec: 0,
+};
+/**
+ *
+ * @param {timer} time
+ */
+export function countdown(time) {
+  if (time.sec <= 0) {
+    time.sec = 60;
+    time.min--;
+  }
+  if (time.min <= 0) {
+    time.min = 60;
+    time.h--;
+    return;
+  };
+  time.sec--;
 }
--- bp/scripts/main.js
+++ bp/scripts/main.js
@@ -1,1 +1,18 @@
 import { world } from "@minecraft/server";
+import { ActionFormData, FormCancelationReason } from "@minecraft/server-ui";
+import { ChestFormData } from "./extensions/forms.js";
+world.afterEvents.itemUse.subscribe((e) => {
+  if (e.itemStack.typeId === "minecraft:compass") {
+    show(e.source);
+  }
+});
+function show(player) {
+  const ui = new ChestFormData()
+    .title("Main")
+    .button(0, "Hi", [], "minecraft:stone")
+    .show(player)
+    .then((r) => {
+      if (r.cancelationReason === FormCancelationReason.UserBusy)
+        return show(player);
+    });
+}
