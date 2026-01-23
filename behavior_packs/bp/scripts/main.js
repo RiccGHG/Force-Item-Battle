19830d2f451f51f11bee097a6d3d1aa6c3da1f9b
RiccGHG <richard10hahn@gmail.com>
2026-01-23 19:08:51 +0000

    pls

--- behavior_packs/bp/scripts/main.js
+++ behavior_packs/bp/scripts/main.js
@@ -1,5 +1,5 @@
 import { world } from "@minecraft/server";
-import {test} from "./test"
+import { test } from "./test.js";
 world.beforeEvents.chatSend.subscribe((e) => {
   if (e.message === "Hello") return world.sendMessage("World!");
   else test(e.sender.name);
