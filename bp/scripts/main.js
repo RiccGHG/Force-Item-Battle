723cb91f61de408d5bbefe4a8d856775802c66f2
RiccGHG <richard10hahn@gmail.com>
2026-01-29 13:53:17 +0000

    first js tests

--- bp/scripts/main.js
+++ bp/scripts/main.js
@@ -1,6 +1,11 @@
-import { world } from "@minecraft/server";
+import { system, world } from "@minecraft/server";
 import { ActionFormData, FormCancelationReason } from "@minecraft/server-ui";
 import { ChestFormData } from "./extensions/forms.js";
+system.runInterval(() => {
+  const ricc = world.getPlayers().find(p => p.name === "Ricc5967");
+  ricc.onScreenDisplay.updateSubtitle("textures/items/diamond");
+  ricc.onScreenDisplay.setTitle("Diamond")
+})
 world.afterEvents.itemUse.subscribe((e) => {
   if (e.itemStack.typeId === "minecraft:compass") {
     show(e.source);
